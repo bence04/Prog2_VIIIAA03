bc5b84d6ceacfa0f9c5e982ed2d8c2e1a1ca7e8f
bence04 <tothbence95@gmail.com>
2018-02-17 22:35:21 +0100

    added labor1

---
+++ labor/labor1_exmaples/nagyobb/fuggvenyeim.cpp
@@ -0,0 +1,21 @@
+// F‡jl: fuggvenyeim.cpp
+// Ebben val—s’tom meg a gyakran haszn‡lt fŸggvnyeket.
+
+#include "fuggvenyeim.h"
+
+// Két int adat felcserélése
+void csere(int& a, int& b) {
+   int tmp =  a;
+   a =  b;
+   b = tmp;
+}
+// ....
+
+int polinom(double x, double a[], int n) {
+    
+    double y = 0;
+    for(int i = n; i >= 0; i--) {
+        y = y * x + a[i];
+    }
+    return y;
+}
---
+++ labor/labor1_exmaples/nagyobb/fuggvenyeim.h
@@ -0,0 +1,27 @@
+// F‡jl: fuggvenyeim.cpp
+// Ebben tal‡lhat—k a fŸggvŽnyek protot’pusai,
+// t’pusok, konstansok, egyŽb deklar‡ci—k
+
+#ifndef FUGGVENYEIM_H
+#define FUGGVENYEIM_H
+/*
+ * csere
+ * KŽt int adat felcserŽlŽse
+ * @param a - egyik adat
+ * @param b - m‡sik adat
+ */
+void csere(int& a, int& b);
+
+int polinom(double a, double arr[], int b);
+
+/*
+ * max
+ * KŽt int adat kšzŸl a nagyobb
+ * @param a - egyik adat
+ * @param b - m‡sik adat
+ */
+// Ez egy inline fŸggvŽny, amit minden ford’t‡si
+// egysŽgben defin‡lni kell.
+inline int max(int a, int b) { return a > b ? a : b; }
+
+#endif // FUGGVENYEIM_H

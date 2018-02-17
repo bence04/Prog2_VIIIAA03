251fe988a557e877039dfff99b66e150cfefa94e
bence04 <tothbence95@gmail.com>
2018-02-17 23:06:48 +0100

    added auth

---
+++ hftest/auth/auth/main.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  auth
+//
+//  Created by Tóth Bence on 2018. 02. 17..
+//  Copyright © 2018. Tóth Bence. All rights reserved.
+//
+
+#include <iostream>
+
+void identify(int task)
+{
+    std::cout << "NÉV <E-mail> NEPTUN" << std::endl;
+    std::cout << "Feladat = " << task << std::endl;
+}
+
+int main(int argc, const char * argv[]) {
+    identify(1);
+    return 0;
